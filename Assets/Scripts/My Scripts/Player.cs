75f61eb6174120048c38d3cf64212a35e6050df9
Misha Chaikovsky <mdchaikovskyi.fitm23@kubg.edu.ua>
2026-06-13 00:41:18 +0300

    lab4-updated

--- Assets/Scripts/My Scripts/Player.cs
+++ Assets/Scripts/My Scripts/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour
     private float start_scale_x;
 
     public Rigidbody2D rb;
-
     public Animator anim;
 
     public float speed = 1f;
@@ -30,7 +29,6 @@ public class Player : MonoBehaviour
 
     public Text HPScore;
     public Text KEScore;
-    
 
     void Start()
     {
@@ -44,82 +42,86 @@ public class Player : MonoBehaviour
         SetScore();
 
         if (Input.GetKeyDown(KeyCode.Space) && on_ground)
-        {
             Jump();
-        }
+
         if (Input.GetMouseButtonDown(0))
         {
             anim.SetFloat("attack", 1);
             Attack();
         }
-        else anim.SetFloat("attack", 0);
+        else
+            anim.SetFloat("attack", 0);
 
         player_obj.transform.Translate(Move(0, 0) * speed * Time.deltaTime);
     }
 
     Vector2 Move(float moveX, float moveY)
     {
-
         if (Input.GetKey(KeyCode.A))
-        {
             moveX = -1f;
-        }
         else if (Input.GetKey(KeyCode.D))
-        {
             moveX = 1f;
-            anim.SetFloat("moveX", Mathf.Abs(player_obj.transform.position.x));
-        }
 
         anim.SetFloat("moveX", Mathf.Abs(moveX));
 
         if (moveX != 0) Rotation(moveX);
-
         return new Vector2(moveX, moveY).normalized;
     }
 
     void Rotation(float moveX)
     {
         player_obj.transform.localScale = new Vector3(
-                start_scale_x * (moveX),
-                player_obj.transform.localScale.y,
-                player_obj.transform.localScale.z
-            );
+            start_scale_x * moveX,
+            player_obj.transform.localScale.y,
+            player_obj.transform.localScale.z
+        );
     }
 
     void Jump()
     {
-        rb.AddForce(Vector2.up * jump_force);
+        rb.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
+        anim.SetBool("on_ground", false);
     }
+
     void CheckingGround()
     {
         on_ground = Physics2D.OverlapCircle(ground_check.position, check_radius, Ground);
         anim.SetBool("on_ground", on_ground);
     }
+
     void Attack()
     {
-        Collider2D got_damage = Physics2D.OverlapBox(enemy_check.position, new Vector2(2f, 1f), 0f, Enemy); 
+        Collider2D hit = Physics2D.OverlapBox(
+            enemy_check.position, new Vector2(2f, 1f), 0f, Enemy
+        );
 
-        if (got_damage != null)
+        if (hit != null)
         {
-            Enemy enemy = got_damage.GetComponent<Enemy>();
+            // Використовуємо TakeDamage щоб активувати анімацію ворога
+            Enemy enemy = hit.GetComponent<Enemy>();
             if (enemy != null)
             {
-                enemy.enemy_hp -= damage;
+                enemy.TakeDamage(damage);
 
-                if (enemy.enemy_hp <= 0)
+                if (enemy.is_dead)
                 {
-                    Destroy(enemy.enemy);
                     killed_enemy += 1;
-
                     if (killed_enemy == 1)
                     {
-                        SceneManager.LoadScene(0);
-                        Time.timeScale = 1f;
+                        StartCoroutine(RestartAfterDelay(2f));
                     }
                 }
             }
         }
     }
+
+    System.Collections.IEnumerator RestartAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
     void SetScore()
     {
         HPScore.text = Convert.ToString(hp);
