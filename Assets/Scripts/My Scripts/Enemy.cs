cfe333c648b772772a3d44715c70b94a83b6946c
Misha Chaikovsky <mdchaikovskyi.fitm23@kubg.edu.ua>
2026-06-13 00:10:28 +0300

    lab3

--- Assets/Scripts/My Scripts/Enemy.cs
+++ Assets/Scripts/My Scripts/Enemy.cs
@@ -1,63 +1,64 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Enemy : MonoBehaviour
+namespace MyGame
 {
-    private GameObject player_obj;
-
-    public GameObject enemy;
-
-    public int attack_damage = 20;
-    public int enemy_hp = 100;
-
-    public float speed = 5f;
-    public float attack_radius = 0.3f;
-    public float attack_cool_down = 1f;
-    public float next_attack_time = 0f;
-
-    public bool player_in_area;
-
-    public Transform player_check;
-
-    public LayerMask Player;
-
-    void Start()
+    public class Enemy : MonoBehaviour
     {
-        player_obj = GameObject.FindWithTag("Player");
-    }
+        private GameObject player_obj;
 
-    void Update()
-    {
-        Following();
-        PlayerAttack();
-    }
+        public GameObject enemy;
 
-    void Following()
-    {
-        if (player_obj.transform.position.x > enemy.transform.position.x)
+        public int attack_damage = 20;
+        public int enemy_hp = 100;
+
+        public float speed = 5f;
+        public float attack_radius = 0.3f;
+        public float attack_cool_down = 1f;
+        public float next_attack_time = 0f;
+
+        public bool player_in_area;
+
+        public Transform player_check;
+        public LayerMask Player;
+
+        void Start()
         {
-            enemy.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            player_obj = GameObject.FindWithTag("Player");
         }
-        else if (player_obj.transform.position.x < enemy.transform.position.x)
+
+        void Update()
         {
-            enemy.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            Following();
+            PlayerAttack();
         }
-    }
-    void PlayerAttack()
-    {
-        player_in_area = Physics2D.OverlapCircle(player_check.position, attack_radius, Player);
 
-        if (player_in_area && Time.time >= next_attack_time)
+        void Following()
         {
-            player_obj.GetComponent<Player>().hp -= attack_damage;
+            if (player_obj == null) return;
+            float dir = player_obj.transform.position.x > enemy.transform.position.x ? 1f : -1f;
+            enemy.transform.position += new Vector3(dir * speed * Time.deltaTime, 0, 0);
+        }
 
-            if (player_obj.GetComponent<Player>().hp <= 0)
+        void PlayerAttack()
+        {
+            player_in_area = Physics2D.OverlapCircle(player_check.position, attack_radius, Player);
+
+            if (player_in_area && Time.time >= next_attack_time)
             {
-                SceneManager.LoadScene(0);
-                Time.timeScale = 1f;
-            }
+                var playerScript = player_obj.GetComponent<Player>();
+                if (playerScript != null)
+                {
+                    playerScript.hp -= attack_damage;
 
-            next_attack_time = Time.time + attack_cool_down;
+                    if (playerScript.hp <= 0)
+                    {
+                        SceneManager.LoadScene(0);
+                        Time.timeScale = 1f;
+                    }
+                }
+                next_attack_time = Time.time + attack_cool_down;
+            }
         }
     }
 }
--- Assets/Scripts/My Scripts/ScreenClamper.cs
+++ Assets/Scripts/My Scripts/ScreenClamper.cs
@@ -1,50 +1,39 @@
 using UnityEngine;
 
-/// <summary>
-/// Keeps object within screen boundaries.
-/// Prevents object from leaving the visible area.
-/// </summary>
-public class ScreenClamper : MonoBehaviour
+namespace MyGame
 {
-    [Header("Settings")]
-    [SerializeField] private bool clampHorizontal = true;
-    [SerializeField] private bool clampVertical = true;
-    [SerializeField] private float padding = 0.5f;
-    
-    private Camera mainCamera;
-    private float minX, maxX, minY, maxY;
-    
-    private void Awake()
+    public class ScreenClamper : MonoBehaviour
     {
-        mainCamera = Camera.main;
-        CalculateScreenBounds();
-    }
-    
-    private void CalculateScreenBounds()
-    {
-        Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0f, 0f, mainCamera.nearClipPlane));
-        Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(1f, 1f, mainCamera.nearClipPlane));
-        
-        minX = bottomLeft.x + padding;
-        maxX = topRight.x - padding;
-        minY = bottomLeft.y + padding;
-        maxY = topRight.y - padding;
-    }
-    
-    private void LateUpdate()
-    {
-        Vector3 position = transform.position;
-        
-        if (clampHorizontal)
+        [Header("Settings")]
+        [SerializeField] private bool clampHorizontal = true;
+        [SerializeField] private bool clampVertical = true;
+        [SerializeField] private float padding = 0.5f;
+
+        private Camera mainCamera;
+        private float minX, maxX, minY, maxY;
+
+        private void Awake()
         {
-            position.x = Mathf.Clamp(position.x, minX, maxX);
+            mainCamera = Camera.main;
+            CalculateScreenBounds();
         }
-        
-        if (clampVertical)
+
+        private void CalculateScreenBounds()
         {
-            position.y = Mathf.Clamp(position.y, minY, maxY);
+            Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0f, 0f, mainCamera.nearClipPlane));
+            Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(1f, 1f, mainCamera.nearClipPlane));
+            minX = bottomLeft.x + padding;
+            maxX = topRight.x - padding;
+            minY = bottomLeft.y + padding;
+            maxY = topRight.y - padding;
         }
-        
-        transform.position = position;
+
+        private void LateUpdate()
+        {
+            Vector3 position = transform.position;
+            if (clampHorizontal) position.x = Mathf.Clamp(position.x, minX, maxX);
+            if (clampVertical) position.y = Mathf.Clamp(position.y, minY, maxY);
+            transform.position = position;
+        }
     }
 }
